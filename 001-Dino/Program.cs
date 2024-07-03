f56affb2596ad397ed4622e54a28af0aa950e474
IsaelGabriel <isaelgsfaria@gmail.com>
2024-07-02 22:56:23 -0300

    Add base for static game class

--- 001-Dino/Program.cs
+++ 001-Dino/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Olá, Mundo!");
+﻿
+using Raylib_cs;
+
+public class Game {
+    public const int WindowWidth = 800;
+    public const int WindowHeight = 400;
+    public const string Title = "Dino";
+    public static readonly Color ClearColor = Color.RayWhite;
+    static void Main() {
+        Raylib.InitWindow(WindowWidth, WindowHeight, Title);
+
+        Raylib.SetTargetFPS(300);
+
+        while(!Raylib.WindowShouldClose()) {
+            Raylib.ClearBackground(ClearColor);
+            Raylib.BeginDrawing();
+
+                Raylib.DrawFPS(0, 0);
+            Raylib.EndDrawing();
+        }
+
+
+        Raylib.CloseWindow();
+    }
+}
