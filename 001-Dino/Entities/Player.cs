f79caf1d07b367ca399b545c55dd9790bf2a41a3
IsaelGabriel <isaelgsfaria@gmail.com>
2024-07-02 23:42:27 -0300

    Improve player jump

--- 001-Dino/Entities/Player.cs
+++ 001-Dino/Entities/Player.cs
@@ -8,8 +8,9 @@ public class Player : IEntity
     public const int Width = 48;
     public const int Height = 2*Width;
 
-    private const float Gravity = 98f;
-    private const float JumpForce = 200f;
+    private const float Gravity = 1200f;
+    private const float JumpVelocity = 700f;
+    private const float JumpDuration = 0.01f;
     private static readonly float defaultY = MainScene.GroundY - Height;
 
     public const int x = 50;
@@ -17,19 +18,21 @@ public class Player : IEntity
 
     private float yVelocity = 0f;
     private bool canJump = true;
+    private float jumpCount = 0f;
 
     public void Update()
     {
+        jumpCount -= Raylib.GetFrameTime();
+
         if(Raylib.IsKeyPressed(KeyboardKey.Space) && canJump) {
-            yVelocity = -JumpForce;
+            yVelocity = -JumpVelocity;
+            jumpCount = JumpDuration;
             canJump = false;
-        }else {
-            float mult = (yVelocity < 0f)? 2f : 1f;
-            yVelocity += Gravity * Raylib.GetFrameTime() * mult;
+        }else if(jumpCount <= 0f) {
+            yVelocity += Gravity * Raylib.GetFrameTime();
         }
 
-        float velMult = (yVelocity > 0f)? 2f : 1f;
-        y += velMult * yVelocity * Raylib.GetFrameTime();
+        y += yVelocity * Raylib.GetFrameTime();
 
         if(y > defaultY) {
             y = defaultY;
